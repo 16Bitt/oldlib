dbe82ca4ed3a567c4ac66bcbcd2de9bfb71db212
Austin Bittinger <itwassocash@gmail.com>
2016-07-21 17:50:51 -0500

    Added install build option, now a shared library

--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 SOURCES	= $(patsubst %.c,%.o,$(wildcard *.c))
-CFLAGS	= -Werror -Wall -std=c99 -g
-TARGET	= oldlib.a
+CFLAGS	= -Werror -Wall -std=c99 -g -shared -fPIC
+TARGET	= liboldlib.so
 
 all: $(SOURCES)
-	$(AR) -rcs $(TARGET) $(SOURCES)
+	$(CC) $(CFLAGS) -o $(TARGET) $(SOURCES)
 
 test: all
 	cd testing && make run
@@ -12,3 +12,8 @@ clean:
 	-cd testing && make clean
 	-rm $(SOURCES)
 	-rm $(TARGET)
+
+install: all
+	cp $(TARGET) /usr/local/lib/
+	cp oldlib.h /usr/local/include/
+	ldconfig
--- oldlib.h
+++ oldlib.h
@@ -13,11 +13,11 @@ enum errorlevels{
 };
 
 //Set the strictness of the warnings and errors
-void oldlib_error_level(int level);
+extern void oldlib_error_level(int level);
 
 //Print out where these happen
-void oldlib_warning_msg(const char* file, const char* function, int line, char* message);
-void oldlib_error_msg(const char* file, const char* function, int line, char* message);
+extern void oldlib_warning_msg(const char* file, const char* function, int line, char* message);
+extern void oldlib_error_msg(const char* file, const char* function, int line, char* message);
 #define OLDLIB_WARN(msg) oldlib_warning_msg(__FILE__, __func__, __LINE__, msg)
 #define OLDLIB_ERR(msg) oldlib_error_msg(__FILE__, __func__, __LINE__, msg)
 #define OLDLIB_ASSERT(xpr) if(!(xpr))oldlib_warning_msg(__FILE__, __func__, __LINE__, "Assertion failed: '" #xpr "'");
@@ -36,21 +36,21 @@ typedef struct linkedlist{
 } linkedlist_t;
 
 //Constructor and destructor
-linkedlist_t* mk_linkedlist();
-void rm_linkedlist(linkedlist_t* list);
+extern linkedlist_t* mk_linkedlist();
+extern void rm_linkedlist(linkedlist_t* list);
 
 //Length
-size_t linkedlist_length(linkedlist_t* list);
+extern size_t linkedlist_length(linkedlist_t* list);
 
 //Getters and setters
-void* linkedlist_get(linkedlist_t* list, size_t index);
-void linkedlist_set(linkedlist_t* list, size_t index, void* value);
+extern void* linkedlist_get(linkedlist_t* list, size_t index);
+extern void linkedlist_set(linkedlist_t* list, size_t index, void* value);
 
 //Other operations
-void linkedlist_insert(linkedlist_t* list, size_t index, void* value);
-void linkedlist_delete(linkedlist_t* list, size_t index);
-void* linkedlist_pop(linkedlist_t* list);
-void linkedlist_push(linkedlist_t* list, void* value);
+extern void linkedlist_insert(linkedlist_t* list, size_t index, void* value);
+extern void linkedlist_delete(linkedlist_t* list, size_t index);
+extern void* linkedlist_pop(linkedlist_t* list);
+extern void linkedlist_push(linkedlist_t* list, void* value);
 
 
 /* Hashmaps */
@@ -79,13 +79,13 @@ typedef struct hashmap{
 } hashmap_t;
 
 //Constructors and destructors
-hashmap_t* mk_hashmap(size_t size);
-hashmap_t* mk_hashmap_custom(size_t size, hashfunction_t fn);
-void rm_hashmap(hashmap_t* hashmap);
+extern hashmap_t* mk_hashmap(size_t size);
+extern hashmap_t* mk_hashmap_custom(size_t size, hashfunction_t fn);
+extern void rm_hashmap(hashmap_t* hashmap);
 
 //Accessors
-void* hashmap_get(hashmap_t* hashmap, char* key);
-bool hashmap_exists(hashmap_t* hashmap, char* key);
-void hashmap_set(hashmap_t* hashmap, char* key, void* value);
+extern void* hashmap_get(hashmap_t* hashmap, char* key);
+extern bool hashmap_exists(hashmap_t* hashmap, char* key);
+extern void hashmap_set(hashmap_t* hashmap, char* key, void* value);
 
 #endif
